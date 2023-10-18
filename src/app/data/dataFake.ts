6412ae5ac0c736a812e5159a358885db729e934e
DavydKaio <davyd.kaio@gmail.com>
2023-10-18 03:00:09 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+	{
+		id: 1,
+		photo:
+			'https://ovicio.com.br/wp-content/uploads/2019/06/20190613-mcu-1-iron-man.jpg',
+		title: 'NOVO HOMEM DE FERRO EM 3D',
+		description: 'Marvel anuncia novo homem de ferro',
+	},
+	{
+		id: 2,
+		photo:
+			'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+		title: 'Nova série anunciada no Disney+',
+		description: 'ola',
+	},
+];
